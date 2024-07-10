38f25fbc0551ef4cf61eec8bae2ce976bc2b59d8
kapiba-ra <kapirei08@outlook.jp>
2024-07-10 13:16:15 +0900

    VECTOR2コメント修正

--- VECTOR2.cpp
+++ VECTOR2.cpp
@@ -15,37 +15,15 @@ VECTOR2 VECTOR2::operator* (float f) const {
     return VECTOR2(x * f, y * f);
 }
 
-//VECTOR2 VECTOR2::operator/ (float f) const {
-//    return VECTOR2(x / f, y / f);
-//}
-
-//VECTOR2 VECTOR2::operator- () const {
-//    return VECTOR2(-x, -y);
-//}
-
 void VECTOR2::operator+= (const VECTOR2& v) {
     x += v.x;
     y += v.y;
 }
 
-//void VECTOR2::operator-= (const VECTOR2& v) {
-//    x -= v.x;
-//    y -= v.y;
-//}
-
 void VECTOR2::operator*= (float f) {
     x *= f;
     y *= f;
 }
-
-//void VECTOR2::operator/= (float f) {
-//    x /= f;
-//    y /= f;
-//}
-
-//VECTOR2 operator* (float f, const VECTOR2& v) {
-//    return VECTOR2(v.x * f, v.y * f);
-//}
 
 float length(const VECTOR2& a) {
     return std::sqrt(a.x * a.x + a.y * a.y);
@@ -62,11 +40,3 @@ VECTOR2 normalize(const VECTOR2& a) {
     }
     return VECTOR2(0, 0);
 }
-
-//float dot(const VECTOR2& a, const VECTOR2& b) {
-//    return a.x * b.x + a.y * b.y;
-//}
-//
-//float cross(const VECTOR2& a, const VECTOR2& b) {
-//    return a.x * b.y - a.y * b.x;
-//}
--- VECTOR2.h
+++ VECTOR2.h
@@ -10,17 +10,10 @@ public:
     VECTOR2 operator+ (const VECTOR2& v) const;
     VECTOR2 operator- (const VECTOR2& v) const;
     VECTOR2 operator* (float f) const;
-    //VECTOR2 operator/ (float f) const;
-    //VECTOR2 operator- () const;
     void operator+= (const VECTOR2& v);
-    //void operator-= (const VECTOR2& v);
     void operator*= (float f);
-    //void operator/= (float f);
 };
 
-//VECTOR2 operator* (float f, const VECTOR2& v);      // VECTOR2 = float * VECTOR2
 float length(const VECTOR2& a);
 float sqLength(const VECTOR2& a);
 VECTOR2 normalize(const VECTOR2& a);
-//float dot(const VECTOR2& a, const VECTOR2& b);
-//float cross(const VECTOR2& a, const VECTOR2& b);
